1e509b75622e9dbd2ebe0d45881b836e587988fc
Zlaam <mrzalaam@gmail.com>
2024-04-03 15:38:04 +0500

    update bun.lockb, package.json, globals.css and page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,77 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 import Link from "next/link";
 import { Button, buttonVariants } from "./components/ui/button";
+import { ArrowDownToLine, CheckCircle, Leaf } from "lucide-react";
+
+const perks = [
+  {
+    name: "Instants Delivery",
+    Icon: ArrowDownToLine,
+    description:
+      "Get Your assests deliver to you in seconds and download them right away ",
+  },
+  {
+    name: "Guaranteed Quality",
+    Icon: CheckCircle,
+    description:
+      " Every assests from our plateform is verified by our team to ensure our higest quality standard. Not Happy? we offer 30-days refund guarantee period",
+  },
+  {
+    name: "For the Planet ",
+    Icon: Leaf,
+    description:
+      "WE've pledged 1% of sales to the preservations and restoration of the natural environment ",
+  },
+];
 
 export default function Home() {
   return (
-    <MaxWidthWrapper>
-      <section className="py-20 mx-auto text-center flex-col flex items-center max-w-3xl">
-        <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-          Your Market Place for high-quality
-          <span className="text-blue-600"> digital assests.</span>
-        </h1>
-        <p className="mt-6 text-lg max-w-prose text-muted-foreground">
-          Welcome to Z-Store. Every assests from our plateform is verified by
-          our team to ensure our higest quality standard
-        </p>
-        <div className="flex flex-col sm:flex-row gap-4 mt-6">
-          <Link href={"/products"} className={buttonVariants()}>
-            Browse Trending{" "}
-          </Link>
-          <Button variant={"ghost"}>Our Quality Promise &rarr;</Button>
-        </div>
+    <>
+      <MaxWidthWrapper>
+        <section className="py-20 mx-auto text-center flex-col flex items-center max-w-3xl">
+          <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+            Your Market Place for high-quality
+            <span className="text-blue-600"> digital assests.</span>
+          </h1>
+          <p className="mt-6 text-lg max-w-prose text-muted-foreground">
+            Welcome to Z-Store. Every assests from our plateform is verified by
+            our team to ensure our higest quality standard
+          </p>
+          <div className="flex flex-col sm:flex-row gap-4 mt-6">
+            <Link href={"/products"} className={buttonVariants()}>
+              Browse Trending
+            </Link>
+            <Button variant={"ghost"}>Our Quality Promise &rarr;</Button>
+          </div>
+        </section>
+        {/* Todo:List Product here  */}
+      </MaxWidthWrapper>
+      <section className="border-t border-gray-200 bg-gray-50">
+        <MaxWidthWrapper className="py-20">
+          <div className="grid grid-cols-1 gap-y-12 sm:grid-cols-2 sm:gap-x-6 lg:grid-cols-3 lg:gap-x-8 lg:gap-y-0">
+            {perks.map((perk) => (
+              <div
+                key={perk.name}
+                className="text-center md:flex md:item-start md:text-left lg:block lg:text-center"
+              >
+                <div className="md:flex-shrink-0 flex justify-center">
+                  <div className="h-16 w-16 flex items-center justify-center rounded-full bg-blue-100 text-blue-900 ">
+                    {<perk.Icon className="w-1/3 h-1/3" />}
+                  </div>
+                </div>
+                <div className="mt-6 md:ml-4 md:mt-0 lg:ml-0 lg:mt-6">
+                  <h3 className="text-base font-medium text-gray-900">
+                    {perk.name}
+                  </h3>
+                  <p className="mt-3 text-sm text-muted-foreground">
+                    {perk.description}
+                  </p>
+                </div>
+              </div>
+            ))}
+          </div>
+        </MaxWidthWrapper>
       </section>
-      {/* Todo:List Product here  */}
-    </MaxWidthWrapper>
+    </>
   );
 }
